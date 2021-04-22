f5251c6aedf12b30883a4c4eb54e0b66a2b8f81d
lingoufan <1400695792@qq.com>
2021-04-22 15:20:09 +0800

    抽取底部公共导航

---
+++ src/utils/dist/axios.dev.js
@@ -0,0 +1,45 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _axios = _interopRequireDefault(require("axios"));
+
+var _vant = require("vant");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+_axios["default"].defaults.baseURL = process.env.NODE_ENV == 'development' ? '//121.43.231.185:8080/api/v1' : '//121.43.231.185:8080/api/v1';
+_axios["default"].defaults.withCredentials = true;
+_axios["default"].defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
+_axios["default"].defaults.headers['token'] = localStorage.getItem('token') || '';
+_axios["default"].defaults.headers.post['Content-Type'] = 'application/json';
+
+_axios["default"].interceptors.response.use(function (res) {
+  if (_typeof(res.data) !== 'object') {
+    _vant.Toast.fail('服务端异常！');
+
+    return Promise.reject(res);
+  }
+
+  if (res.data.resultCode != 200) {
+    if (res.data.message) _vant.Toast.fail(res.data.message);
+
+    if (res.data.resultCode == 416) {
+      window.vRouter.push({
+        path: '/login'
+      });
+    }
+
+    return Promise.reject(res.data);
+  }
+
+  return res.data;
+});
+
+var _default = _axios["default"];
+exports["default"] = _default;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,13 @@
-<template>
-  <div class="home">
-      <!-- <van-button type="primary" >默认按钮</van-button>
-      我好 -->
-      <!-- <div>你好</div> -->
-      <div>
-      <i class="iconfont icon-emo"></i>
-      </div>
-  </div>
-</template>
 
-<script>
-export default {
-  name: 'Home',
-  
-}
-</script>
-<style lang="less" scoped>
-.home {
-    div {
-        background: grey;
-        width: 150px;
-        height: 150px;
-        font-size: 18px;
-        i {
-            color: red;
-        }
-    }
-    // color: red;
-    
-}
-</style>
+<template>  
+<div class="home">    
+    <div>我是首页</div>  
+    </div>
+</template>
+    <script>
+        export default { 
+             name: 'Home',
+             };
+    </script>
+<style lang="less" scoped></style>
+               
